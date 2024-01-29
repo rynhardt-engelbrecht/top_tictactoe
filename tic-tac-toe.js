d5ed3fe60950e22baed49779ddbf188519d2d1c1
Rynhardt Engelbrecht <107496937+rynhardt-engelbrecht@users.noreply.github.com>
2024-01-29 14:06:08 +0200

    feat: add createGameboard factory function
    Added the createGameboard factory function to create a Gameboard object,
    containing an array to store the state of the game, and two methods to
    be able to update the board or to retrieve the contents of the board.

---
+++ tic-tac-toe.js
@@ -0,0 +1,33 @@
+/*
+Factory function to create a Gameboard object, containing a board array
+used to store the contents fo the game board, and two methods to be able
+to update the board or get the contents of the board.
+*/
+function createGameboard () {
+  /*
+  rowIndex = Math.trunc(i);
+  colIndex = i % 3;
+  */
+  const board = [
+    0, 0, 0,
+    0, 0, 0,
+    0, 0, 0
+  ];
+
+  const getBoard = () => board;
+  const updateBoard = function(index, symbol) {
+    board[index] = symbol;
+
+    return board;
+  };
+
+  return { getBoard, updateBoard };
+};
+
+// function createPlayer (name, symbol) {
+//   return { name, symbol }
+// }
+
+// function createGame (boardObj, players) {
+//   return { boardObj, players }
+// }
