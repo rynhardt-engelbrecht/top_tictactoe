e71da43a89de43819c539bf9ecca7922186ddb0d
Rynhardt Engelbrecht <107496937+rynhardt-engelbrecht@users.noreply.github.com>
2024-01-29 16:55:42 +0200

    feat: add gameController object
    Added the gameController object to observe the state of the game, and
    allowing players to interface with the gameBoard object.

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -14,7 +14,6 @@ const board = (function createGameboard () {
     0, 0, 0
   ];
 
-  const getBoard = () => board;
   const updateBoard = function(index, num) {
     if (index < board.length && index >= 0) {
       board[index] = num;
@@ -25,19 +24,83 @@ const board = (function createGameboard () {
     return board;
   };
 
-  return { getBoard, updateBoard };
+  return { board, updateBoard };
 })();
 
 /*
 Function to create player objects to interface with the Gameboard object
 */
-function createPlayer (name, num) {
+function createPlayer (num) {
+  const getName = () => this.name;
+  const setName = function(name) {
+    this.name = name;
+  }
   const getNum = () => num;
   const getMove = (index) => index;
 
-  return { name, getNum, getMove };
+  return { getName, setName, getNum, getMove };
 }
 
-// function createGame (boardObj, players) {
-//   return { boardObj, players }
-// }
+const gameController = (function createGameController () {
+  const boardObj = board;
+  const players = [createPlayer(1), createPlayer(2)];
+  const gameWon = function () {
+    return checkRows() || checkCols() || checkDiagonals();
+  };
+  /*
+  Below is stupid math because I decided to use a 1-Dimensional array to represent
+  a 2-Dimensional board.
+  */
+  const checkRows = function () {
+    for (let i = 0; i <= 6; i += 3) {
+
+      const row = [];
+
+      for (let j = 0; j <= 2; j++) {
+        const index = i + j;
+        row.push(boardObj.board[index]);
+      }
+
+      return allEquals(row);
+    }
+  };
+  const checkCols = function () {
+    for (let i = 0; i <= 2; i++) {
+
+      const row = [];
+
+      for (let j = 0; j <= 6; j += 3) {
+        const index = i + j;
+        row.push(boardObj.board[index]);
+      }
+
+      return allEquals(row);
+    }
+  };
+  const checkDiagonals = function () {
+    let row = [];
+
+    for (let i = 0; i <= 8; i += 4) {
+      row.push(boardObj.board[i]);
+    }
+
+    if (allEquals(row)) {
+      return true
+    }
+
+    row = [];
+
+    for (let j = 2; j <= 6; j += 2) {
+      row.push(boardObj.board[j]);
+    }
+
+    return allEquals(row);
+  };
+  const allEquals = function (array) {
+    return array.every((val) => {
+      return (val === array[0] && val != 0);
+    });
+  };
+
+  return { boardObj, players, gameWon };
+})();
